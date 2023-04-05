89a9dc7cd957a686c5d7193a43fe97e149568470
Svi-Kos <svitlanakos@gmail.com>
2023-04-05 22:15:32 +0300

    register&login screens added

--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -1,11 +1,84 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Button,
+} from "react-native";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
+  const [email, onChangeEmail] = useState("");
+  const [password, onChangePassword] = useState("");
+
+  const image = { uri: "https://picsum.photos/id/457/3333/3333" };
+
+  const user = { email, password };
+
+  const handleSubmit = () => {
+    Keyboard.dismiss();
+
+    onChangeEmail("");
+    onChangePassword("");
+
+    // dispatch(authSignIn(user));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <TouchableWithoutFeedback onPress={handleSubmit}>
+          <View style={styles.form}>
+            <View style={styles.header}>
+              <Text style={styles.headerTitle}>Hello again, </Text>
+              <Text style={styles.headerTitle}>Welcome back!</Text>
+            </View>
+            <View>
+              <Text style={styles.inputTitle}>EMAIL</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                value={email}
+                onChangeText={onChangeEmail}
+              />
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.inputTitle}>PASSWORD</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                secureTextEntry={true}
+                value={password}
+                onChangeText={onChangePassword}
+              />
+            </View>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.btn}
+              onPress={handleSubmit}
+            >
+              <Text style={styles.btnTitle}>SIGN IN</Text>
+            </TouchableOpacity>
+            <View style={{ marginHorizontal: 60 }}>
+              <Button
+                color="transparent"
+                title="Go to Register"
+                onPress={() => navigation.navigate("Register")}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -14,7 +87,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "#696969",
+    height: 40,
+    borderRadius: 6,
+    color: "#696969",
+  },
+  form: {
+    marginHorizontal: 40,
+  },
+  btn: {
+    height: 40,
+    borderRadius: 6,
+    marginTop: 40,
+    marginBottom: 20,
+    justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#ff8c00",
+  },
+  inputTitle: {
+    color: "#696969",
+    marginBottom: 10,
+  },
+  btnTitle: {
+    color: "#696969",
+    fontSize: 18,
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 50,
+  },
+  headerTitle: {
+    // fontFamily: "Inter-Black",
+    fontSize: 28,
+    color: "#696969",
   },
 });
 
--- screens/auth/RegisterScreen.js
+++ screens/auth/RegisterScreen.js
@@ -1,11 +1,93 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Button,
+} from "react-native";
 
-const RegisterScreen = () => {
+const RegisterScreen = ({ navigation }) => {
+  const [email, onChangeEmail] = useState("");
+  const [password, onChangePassword] = useState("");
+  const [nickname, onChangeNickname] = useState("");
+
+  const user = { nickname, email, password };
+
+  const image = { uri: "https://picsum.photos/id/120/3333/3333" };
+
+  const handleSubmit = () => {
+    Keyboard.dismiss();
+
+    onChangeEmail("");
+    onChangePassword("");
+    onChangeNickname("");
+    // dispatch(authSignUp(user));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Register</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <TouchableWithoutFeedback onPress={handleSubmit}>
+          <View style={styles.form}>
+            <View style={styles.header}>
+              <Text style={styles.headerTitle}>Registration</Text>
+            </View>
+            <View>
+              <Text style={styles.inputTitle}>NICKNAME</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                value={nickname}
+                onChangeText={onChangeNickname}
+              />
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.inputTitle}>EMAIL</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                value={email}
+                onChangeText={onChangeEmail}
+              />
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.inputTitle}>PASSWORD</Text>
+              <TextInput
+                style={styles.input}
+                textAlign={"center"}
+                secureTextEntry={true}
+                value={password}
+                onChangeText={onChangePassword}
+              />
+            </View>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.btn}
+              onPress={handleSubmit}
+            >
+              <Text style={styles.btnTitle}>SIGN UP</Text>
+            </TouchableOpacity>
+            <View style={{ marginHorizontal: 60 }}>
+              <Button
+                color="transparent"
+                title="Go to Login"
+                onPress={() => navigation.navigate("Login")}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -14,7 +96,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "#f0ffff",
+    height: 40,
+    borderRadius: 6,
+    color: "#f0ffff",
+  },
+  form: {
+    marginHorizontal: 40,
+  },
+  btn: {
+    height: 40,
+    borderRadius: 6,
+    marginTop: 40,
+    marginBottom: 20,
+    justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#483d8b",
+  },
+  inputTitle: {
+    color: "#f0ffff",
+    marginBottom: 10,
+  },
+  btnTitle: {
+    color: "#f0ffff",
+    fontSize: 18,
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 50,
+  },
+  headerTitle: {
+    // fontFamily: "Inter-Black",
+    fontSize: 28,
+    color: "#f0ffff",
   },
 });
 
