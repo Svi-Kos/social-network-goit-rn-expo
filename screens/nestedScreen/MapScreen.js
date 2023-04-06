e0f2e06356d2135b763e2458fa3e9f149b34630e
Svi-Kos <svitlanakos@gmail.com>
2023-04-06 15:36:19 +0300

    map added

--- screens/nestedScreen/MapScreen.js
+++ screens/nestedScreen/MapScreen.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-const MapScreen = () => {
+const MapScreen = ({ route }) => {
+  const { latitude, longitude } = route.params.location;
   return (
     <View style={styles.container}>
-      <Text>Map</Text>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude,
+          longitude,
+          latitudeDelta: 0.006,
+          longitudeDelta: 0.006,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude,
+            longitude,
+          }}
+        />
+      </MapView>
     </View>
   );
 };
@@ -14,7 +31,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center",
   },
 });
 
